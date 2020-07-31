15d637ccca883e8ecbd69a10d01e2f6a5aa54b55
Stoyan Spasov <spasovstoyan@gmail.com>
2020-07-31 13:41:12 +0300

    new update

---
+++ js/data.js
@@ -0,0 +1,138 @@
+function host(endpoint) {
+    return `https://api.backendless.com/04B757E2-F823-EBAE-FF31-DAB0AA48A900/8F6C597D-EF79-40B3-B03F-F5D4B2BE80C8/${endpoint}`
+}
+
+const endpoinsts = {
+
+    REGISTER: 'users/user',
+    LOGIN: 'users/login',
+    LOGOUT: 'users/logout',
+    MOVIES: 'data/movies',
+    MOVIE_BY_ID: 'data/movies/'
+};
+
+async function register(username, password) {
+    return (await fetch(host(endpoinsts.REGISTER), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    })).json();
+}
+
+async function login(username, password) {
+    const result = await (await fetch(host(endpoinsts.LOGIN), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            login: username,
+            password
+        })
+    })).json();
+
+    localStorage.setItem('userToken', result['user-token']);
+    localStorage.setItem('username', result.username);
+    localStorage.setItem('userId', result.objectId);
+
+    return result;
+}
+
+function logout() {
+    const token = localStorage.getItem('userToken')
+    return fetch(host(endpoinsts.LOGOUT), {
+        headers: {
+            'user-token': token
+        }
+    });
+}
+
+//get all movies
+async function getMovies() {
+    const token = localStorage.getItem('userToken')
+
+    return (await fetch(host(endpoinsts.MOVIES), {
+        headers: {
+            'user-token': token
+        }
+    })).json();
+}
+
+//get movie by ID
+async function getMovieById(id) {
+    const token = localStorage.getItem('userToken');
+
+    return (await fetch(host(endpoinsts.MOVIE_BY_ID + id), {
+        headers: {
+            'user-token': token
+        }
+    })).json();
+}
+
+//create movie
+async function createMovie(movie) {
+    const token = localStorage.getItem('userToken');
+
+    return (await fetch(host(endpoinsts.MOVIES), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        },
+        body: JSON.stringify(movie)
+    })).json();
+}
+
+//edit movie
+async function updateMovie(id, updatedProps) {
+    const token = localStorage.getItem('userToken');
+
+    return (await fetch(host(endpoinsts.MOVIE_BY_ID + id), {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        },
+        body: JSON.stringify(updatedProps)
+    })).json();
+}
+
+//delete movie
+async function deleteMovie(id) {
+    const token = localStorage.getItem('userToken');
+
+    return (await fetch(host(endpoinsts.MOVIE_BY_ID + id), {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        }
+    })).json();
+}
+
+//get movies by userID
+async function getMoviesByOwner(ownerId) {
+    const token = localStorage.getItem('userToken');
+
+    return (await fetch(host(endpoinsts.MOVIES + `?where=ownerId%3D%27${ownerId}%27`), {
+        headers: {
+            'Content-Type': 'application/json',
+            'user-token': token
+        }
+    })).json();
+}
+
+//buy ticket
+async function byTicket(movie) {
+
+    const newTickets = movie.tickets - 1;
+    const movieId = movie.objectId;
+
+    return updateMovie(movieId, {tickets: {newTickets}});
+}
+
